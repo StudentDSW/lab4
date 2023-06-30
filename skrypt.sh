200475122825ab6f37ac31d20541474cdee0664b
StudentDSW <dsw49519@student.dsw.edu.pl>
2023-06-30 18:38:18 +0200

    Dodanie pojedynczych flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
   echo "Dzisiejsza data: $(date)"
-fi
 
-if [[ "$1" == "--logs" ]]; then
-  	if [-n "$2" ]; then
-		count="$2"
-	else
-		count=100
-	fi
-	for ((i=1; i<=count;i++));do
-    		filename="log${i}.txt"
-    		echo "Nazwa pliku: $filename" >> "$filename"
-    		echo "Nazwa skryptu: skrypt.sh" >> "$filename"
-    		echo "Data: $(date)" >> "$filename"
-  	done
-fi
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  if [[ -n "$2" ]]; then
+    num_files=$2
+  else
+    num_files=100
+  fi
 
-if [[ "$1" == "--help" ]]; then
-  	echo "Dostępne opcje:"
-  	echo "--date: Wyświetla dzisiejszą datę."
-  	echo "--logs <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
+  for ((i=1; i<=num_files; i++)); do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: $filename" >> "$filename"
+    echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+    echo "Data: $(date)" >> "$filename"
+  done
+
+elif [["$1" == "--init" || "$1" == "-i"]]; then
+	git clone https://github.com/StudentDSW/lab4.git
+	export PATH"$PWD:$PATH"
+	echo "Repozytorium zostało sklonowane do bieżącego katalogu"
+	echo "Ścieżka do repozytorium została dodana do zmiennej środowiskowej PATH"
+
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Dostępne opcje:"
+  echo "--date, -d: Wyświetla dzisiejszą datę."
+  echo "--init, -i: Klonuje repozytorium i dodaje zmienną środowiskową PATH"
+  echo "--logs, -l <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
+
 else
-  	echo "Nieznana komenda. Użyj 'skrypt.sh --help' aby wyświetlić dostępne opcje wyboru."
+  echo "Nieznana komenda. Użyj 'skrypt.sh -h' aby wyświetlić dostępne opcje wyboru."
 fi
